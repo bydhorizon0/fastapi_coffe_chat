e0e1b39143903c0cdd5414a585fd9aa50e1a5710
bydhorizon0 <bydhorizon0@gmail.com>
2026-03-10 01:23:43 +0900

    lifespan 추가

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 import app.domain.models
 from app.domain.account import account_router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("앱 시작")
+    yield
+    print("앱 종료")
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 def include_routers(_app: FastAPI):

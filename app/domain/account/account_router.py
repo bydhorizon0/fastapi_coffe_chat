fc2b8f26893e80ae9757c94fcb93a55a8f0afaa0
bydhorizon0 <bydhorizon0@gmail.com>
2026-03-10 01:26:33 +0900

    가입 라우터 & 스키마 정의

--- app/domain/account/account_router.py
+++ app/domain/account/account_router.py
@@ -1,24 +1,41 @@
-from datetime import datetime, timezone
-from typing import Annotated
-
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, HTTPException, status
 from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession
-from starlette import status
 
-from app.database import get_db
+from app.database import DbSessionDep
+from app.domain.account.exceptions import DuplicatedEmail, DuplicatedUsername
 from app.domain.account.models import User
-from app.domain.account.schema import UserResponse
+from app.domain.account.schema import UserCreate, UserResponse
 
 router = APIRouter(prefix="/account")
 
 
 @router.get("/users/{username}", response_model=UserResponse)
-async def user_detail(username: str, db: Annotated[AsyncSession, Depends(get_db)]) -> dict:
+async def user_detail(username: str, db: DbSessionDep):
     result = await db.execute(select(User).where(User.username == username))
     user = result.scalar_one_or_none()
 
     if user is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+    return user
+
+
+@router.get("/signup", response_model=UserResponse)
+async def signup(body: UserCreate, db: DbSessionDep):
+    existing_user: User = await db.scalar(
+        select(User).where((User.email == body.email) | (User.username == body.username))
+    )
+
+    if existing_user:
+        if existing_user.username == body.username:
+            raise DuplicatedUsername
+        else:
+            raise DuplicatedEmail
+
+    user = User(**body.model_dump(exclude={"password_repeat"}))
+    db.add(user)
+
+    await db.commit()
+    await db.refresh(user)
 
     return user
--- app/domain/account/schema.py
+++ app/domain/account/schema.py
@@ -1,9 +1,49 @@
-from pydantic import BaseModel, ConfigDict, EmailStr
+import random
+import string
+from typing import Self
+
+from pydantic import BaseModel, ConfigDict, EmailStr, Field, field_validator, model_validator
 
 
 class UserResponse(BaseModel):
     username: str
     email: EmailStr
     display_name: str
+    is_host: bool
 
     model_config = ConfigDict(from_attributes=True)
+
+
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    display_name: str | None = Field(min_length=4, max_length=40, description="사용자 표시명")
+    password: str
+    password_repeat: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+    @field_validator("username")
+    @classmethod
+    def username_valid(cls, v: str) -> str:
+        if v.isdigit():
+            raise ValueError("username은 숫자만으로 구성될 수 없습니다.")
+        if len(v) > 40:
+            raise ValueError("username은 40자를 넘을 수 없습니다.")
+        return v
+
+    @model_validator(mode="before")
+    @classmethod
+    def generate_display_name(cls, data: dict):
+        if not data.get("display_name"):
+            data["display_name"] = "".join(
+                random.choices(string.ascii_letters + string.digits, k=8)
+            )
+
+        return data
+
+    @model_validator(mode="after")
+    def password_match(self) -> Self:
+        if self.password != self.password_repeat:
+            raise ValueError("비밀번호가 일치하지 않습니다.")
+        return self
